e00e7253649ab443d4d7426ef1c83a244c8d17d6
zavaletaosv <osvzavaleta@gmail.com>
2021-06-22 17:55:55 -0500

    JS updated: inputs stored to local storage

--- script.js
+++ script.js
@@ -12,3 +12,15 @@ $(".saveBtn").on("click", function() {
     var hour = $(this).parent().attr("id");
     localStorage.setItem(hour, textArea);
 })
+
+// Used to Store Inputs to Local Storage
+
+$("#hour0900 .description").val(localStorage.getItem("hour0900"));
+$("#hour1000 .description").val(localStorage.getItem("hour1000"));
+$("#hour1100 .description").val(localStorage.getItem("hour1100"));
+$("#hour1200 .description").val(localStorage.getItem("hour1200"));
+$("#hour1300 .description").val(localStorage.getItem("hour1300"));
+$("#hour1400 .description").val(localStorage.getItem("hour1400"));
+$("#hour1500 .description").val(localStorage.getItem("hour1500"));
+$("#hour1600 .description").val(localStorage.getItem("hour1600"));
+$("#hour1700 .description").val(localStorage.getItem("hour1700"));
