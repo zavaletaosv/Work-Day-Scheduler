774423ab62331b3c0631c3089de81ecee4c3676f
zavaletaosv <osvzavaleta@gmail.com>
2021-06-22 17:53:32 -0500

    JS updated: event listener created for save button

--- script.js
+++ script.js
@@ -4,3 +4,11 @@ $(document).ready(function() {
     var currentTime = moment().format("dddd, MMMM Do YYYY");
     $("#currentDay").text(currentTime);
 })
+
+// Event Listener: Save Button
+
+$(".saveBtn").on("click", function() {
+    var textArea = $(this).siblings(".description").val();
+    var hour = $(this).parent().attr("id");
+    localStorage.setItem(hour, textArea);
+})
